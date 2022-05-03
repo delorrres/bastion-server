9b8f1c887636cab933b68b736e85b8b49aa4a5ab
Delorrres <deloresmawuse@gmail.com>
2022-05-03 09:25:09 -0500

    UPDATE- error fix

--- instance.tf
+++ instance.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "bastion" {
     instance_type = var.ec2_type
     key_name = var.my_keypair
     subnet_id = data.aws_subnet.public-elk.id
-    vpc_security_group_ids = [aws_security_group.my_private_app_sg.id]
+    vpc_security_group_ids = [aws_security_group.my_public_app_sg.id]
     availability_zone = "us-east-2a"
     associate_public_ip_address = true
 
